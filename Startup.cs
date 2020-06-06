37b8f248a11f9979bcbd0bf1748cacda419ff619
KJnong <57115252+KJnong@users.noreply.github.com>
2020-06-06 03:42:27 +0200

    First commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChooseEvent2.Startup))]
+namespace ChooseEvent2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
