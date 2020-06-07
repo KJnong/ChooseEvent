77882fee6e5c64e8744d318db4d902740817b88f
KJnong <57115252+KJnong@users.noreply.github.com>
2020-06-07 03:47:27 +0200

    Create Gig and Genre model

---
+++ Models/Gig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChooseEvent2.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Vanue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+    }
+}
