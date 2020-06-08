bbd37fdac5751cce3d6584c88129f25f658c7838
KJnong <57115252+KJnong@users.noreply.github.com>
2020-06-08 02:58:24 +0200

    added validation for GigsViewModel

--- ViewModels/GigsViewModel.cs
+++ ViewModels/GigsViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,20 +10,25 @@ namespace ChooseEvent2.ViewModels
 {
     public class GigsViewModel
     {
-
+        [Required]
         public string Date { get; set; }
 
+        [Required]
         public string Time { get; set; }
 
+        [Required]
         public DateTime DateTime { get 
             {
                 return DateTime.Parse(String.Format("{0} {1}", Date, Time));
             } }
 
+        [Required]
         public string Venue { get; set; }
 
+        [Required]
         public byte Genre { get; set; }
 
+        [Required]
         public IEnumerable<Genre> Genres { get; set; }
 
     }
