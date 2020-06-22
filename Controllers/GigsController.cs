f2f083a81aa74cef4d01e82c78b6641210d8f1d1
KJnong <nongjethro@gmail.com>
2020-06-22 16:47:17 +0200

    Implemented update in CRUD

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -23,10 +23,33 @@ namespace ChooseEvent2.Controllers
         {
             var ViewModel = new GigsViewModel
             {
-                Genres = db.Genres.ToList()
+                Genres = db.Genres.ToList(),
+                Heading = "Add Gig"
+
             };
             
-            return View(ViewModel);
+            return View("GigForm", ViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GigsViewModel viewModel)
+        {
+
+            var gig = new Gig()
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.DateTime,
+                Genreid = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            db.Gigs.Add(gig);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Mine", "Gigs");
         }
 
         [Authorize]
@@ -50,26 +73,68 @@ namespace ChooseEvent2.Controllers
             return View("Gigs", Gigs);
         }
 
+        [Authorize]
+        public ActionResult ArtistFollowing()
+        {
+            var UserId = User.Identity.GetUserId();
+            var artistFollowing = db.Relationships
+                .Where(a => a.FolloweeId == UserId).Select(a => a.Follower).ToList();
+
+
+            Console.WriteLine("Hellow ");
+            return View(artistFollowing);
+        }
+
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var UserId = User.Identity.GetUserId();
+            var gigs = db.Gigs
+                .Where(g => g.ArtistId == UserId && g.DateTime > DateTime.Now).Include(g => g.Genre).ToList();
+
+            return View(gigs);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var UserId = User.Identity.GetUserId();
+            var gig = db.Gigs.Single(g => g.Id == id && g.ArtistId == UserId);
+
+            var ViewModel = new GigsViewModel
+            {
+                Genres = db.Genres.ToList(),
+                Id = gig.Id,
+                Date = gig.DateTime.ToString("d MMM yyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.Genreid,
+                Venue = gig.Venue,
+                Heading = "Edit Gig"
+            };
+
+            return View("GigForm", ViewModel);
+        }
 
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(GigsViewModel viewModel)
+        public ActionResult Update(GigsViewModel viewModel)
         {
-         
-            var gig = new Gig()
-            {
-                ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
-                Genreid = viewModel.Genre,
-                Venue = viewModel.Venue
-            };
+            var UserId = User.Identity.GetUserId();
+            var gig = db.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == UserId);
 
-            db.Gigs.Add(gig);
+
+            gig.DateTime = viewModel.DateTime;
+            gig.Genreid = viewModel.Genre;
+            gig.Venue = viewModel.Venue;
+           
             db.SaveChanges();
 
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
+
+
     }
 }
--- ViewModels/GigsViewModel.cs
+++ ViewModels/GigsViewModel.cs
@@ -1,15 +1,20 @@
-﻿using ChooseEvent2.Models;
+﻿using ChooseEvent2.Controllers;
+using ChooseEvent2.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ChooseEvent2.ViewModels
 {
     public class GigsViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         [FutureDate]
         public string Date { get; set; }
@@ -32,6 +37,20 @@ namespace ChooseEvent2.ViewModels
 
         [Required]
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action { 
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update = (c => c.Update(null));
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(null));
+
+                var action = (Id != 0) ? update : create;
+
+                return (action.Body as MethodCallExpression).Method.Name;
+            }
+        }
 
     }
 }
